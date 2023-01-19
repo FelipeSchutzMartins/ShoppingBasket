e586db7124c790cc52f3667df8e32f010ae09e28
Felipe <felipeschutz2025@hotmail.com>
2023-01-19 13:57:01 -0300

    :hammer: removed some comments

--- src/test/java/com/interview/shoppingbasket/BasketStubs.java
+++ src/test/java/com/interview/shoppingbasket/BasketStubs.java
@@ -4,11 +4,11 @@ public class BasketStubs {
 
     public static Basket buildSimpleBasket() {
         Basket basket = new Basket();
-        basket.add("productCode", "productCode", 11); //3.99
+        basket.add("productCode", "productCode", 11);
         basket.add("productCode2", "productCode2", 10);
         basket.add("productCode2", "productCode2", 15);
-        basket.add("productCode2", "productCode2", 20); //2.0
-        basket.add("productCode3", "productCode3", 15); // 5.0
+        basket.add("productCode2", "productCode2", 20);
+        basket.add("productCode3", "productCode3", 15);
         return basket;
     }
 }
